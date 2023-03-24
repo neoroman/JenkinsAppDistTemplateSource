087d1eca4d1cb891401dd5238246c2f124182e8e
EungShik <neoroman>
2023-03-24 13:08:45 +0900

    [bugfix] history back step 1 not 2 after upload binary to App Store

--- ios/appstore_upload.php
+++ ios/appstore_upload.php
@@ -19,11 +19,11 @@ if (isset($_SERVER['HTTP_REFERER'])) {
 if ($isDebugMode) {
     $output = shell_exec(__DIR__ .'/AppStoreUpload.sh -d -p ios -f ' . escapeshellarg($file_title)) . ' -r ' . escapeshellarg($root);
     echo 'INPUT => '. __DIR__ .'/AppStoreUpload.sh -d -p ios -f ' . escapeshellarg($file_title) . ' -r ' . escapeshellarg($root) .'<BR /><BR />' . $_SERVER['HTTP_REFERER'];
-    exit("<BR /><BR /><pre>$output</pre><BR />iOS upload to App Store .... [ DEBUG ]<br /><a href='javascript:window.history.go(-2);'>뒤로가기</a>");
+    exit("<BR /><BR /><pre>$output</pre><BR />iOS upload to App Store .... [ DEBUG ]<br /><a href='javascript:window.history.go(-1);'>뒤로가기</a>");
 } else {
     $output = shell_exec(__DIR__ .'/AppStoreUpload.sh -p ios -f ' . escapeshellarg($file_title)) . ' -r ' . escapeshellarg($root);
     echo 'INPUT => '. __DIR__ .'/AppStoreUpload.sh -p ios -f ' . escapeshellarg($file_title) . ' -r ' . escapeshellarg($root) .'<BR /><BR />' . $_SERVER['HTTP_REFERER'];
     // exit("<META http-equiv='REFRESH' content='1;url=$referrer'>$output<BR />iOS upload to App Store  [ DONE ]<br /><a href='javascript:window.history.go(-2);'>뒤로가기</a>");
-    exit("<BR /><BR /><pre>$output</pre><BR />iOS upload to App Store  [ DONE ]<br /><a href='javascript:window.history.go(-2);'>뒤로가기</a>");
+    exit("<BR /><BR /><pre>$output</pre><BR />iOS upload to App Store  [ DONE ]<br /><a href='javascript:window.history.go(-1);'>뒤로가기</a>");
 }
 ?>
